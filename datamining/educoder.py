d14f2b8a6c4d6a0186c0d930dfdc84b6fa7d5bf0
sisterofDC <90042818+sisterofDC@users.noreply.github.com>
2022-03-09 08:25:54 +0800

    homework

---
+++ datamining/educoder.py
@@ -0,0 +1,26 @@
+import keyword
+
+# 错误1
+str1 = "string"
+print(str1)
+
+# 错误2
+test = 1024
+print(test)
+
+# 错误3
+float = 1.024
+print(float)
+
+# 错误3
+false = False
+print(false)
+print(keyword.kwlist)
+# 在此处输出保留关键字
+
+print("end")
+
+
+
+
+
---
+++ datamining/onehomework.py
@@ -0,0 +1,13 @@
+# 1, 2, 3, 4, 5
+# a, b, c, d
+# physics, chemistry, 1997, 2000
+
+def one():
+    one = [1,2,3,4,5]
+    two = ['a', 'b', 'c', 'd']
+    three = ["physics", "chemistry", 1997, 2000]
+    print(one,two,three)
+
+
+if __name__ == '__main__':
+    one()
